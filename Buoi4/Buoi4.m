70121b3fcc0070c857b97318aa9c407364a658a6
lamphucnghi313 <lamphucnghi@gmail.com>
2020-09-21 12:41:54 +0700

    no message

--- Buoi4/Buoi4.m
+++ Buoi4/Buoi4.m
@@ -7,6 +7,15 @@ clc; clear all; format long;
 % disp(['Bai 1, S = ', num2str(S)])
 
 %% Bai 2. Dat Sn = sigma(k), k=1..n. Tinh sigma(Sn),n=1..10
+% s=0;
+% for n=1:10
+%     for k=1:n
+%         s = s + k;
+%     end
+% end
+% disp(s)
+
+
 % s1 = 0;
 % for n=1:10
 %     s2 = 0;
@@ -16,14 +25,6 @@ clc; clear all; format long;
 %     s1 = s1 + s2;
 % end
 % disp(['Bai 2, S = ', num2str(s1)])
-% 
-% s=0;
-% for n=1:10
-%     for k=1:n
-%         s = s + k;
-%     end
-% end
-% disp(s)
 
 %% Bai 3. Dung for..end tinh tong cac so chia het cho 3
 %                                          va nho hon 50.
@@ -43,6 +44,5 @@ clc; clear all; format long;
 %     S = S + k*k;
 % end
 % k
-
 
 
