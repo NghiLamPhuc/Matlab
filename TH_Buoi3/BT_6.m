8da3baae4fa16e184f0fc0525ec63b04d96c5103
lamphucnghi313 <lamphucnghi@gmail.com>
2020-10-13 12:47:00 +0700

    no message

--- TH_Buoi3/BT_6.m
+++ TH_Buoi3/BT_6.m
@@ -132,27 +132,21 @@ format long
 % A(2, :) = A(2, :) - 2 * A(1, :)
 
 %% Bai tap 6
-B = [1 -1 5 -1; 1 1 -2 3; 3 -1 8 1; 1 3 -9 7];
-% rats(rref(B))
+% B = [1 -1 5 -1; 1 1 -2 3; 3 -1 8 1; 1 3 -9 7]
+% % rref(B)
+% % rats(rref(B))
 % % Bien doi so cap tren dong
-% B(2,:) = B(2,:) - B(1,:)
-% B(3,:) = B(3,:) - 3*B(1,:)
-% B(4,:) = B(4,:) - B(1,:)
+% B(2,:) = B(2,:) + (-1)*B(1,:)
+% B(3,:) = B(3,:) + (-3)*B(1,:)
+% B(4,:) = B(4,:) + (-1)*B(1,:) 
 % 
-% % Ma tran don vi cung kich thuoc B
-% B_2_4 = eye(4)
-% % % Doi vi tri dong 2 dong 4 ma tran don vi
-% temp = B_2_4(2,:);
-% B_2_4(2,:) = B_2_4(4,:)
-% B_2_4(4,:) = temp
-% % % Doi vi tri dong 2 dong 4 ma tran B
-% B = B_2_4 * B
-% % % Bien doi so cap tren dong
-% B(3,:) = 2*B(3,:) - B(2,:);
-% B(4,:) = 2*B(4,:) - B(2,:)
-% B(2,:) = B(2,:)/4
+% B(2,:) = B(2,:)/2
+% B(3,:) = B(3,:) + (-2)*B(2,:)
+% B(4,:) = B(4,:) + (-4)*B(2,:)
+% 
+% B(1,:) = B(1,:) + B(2,:)
 % rats(B) % Hien thi B theo pha^n so.
-% 
+% % 
 % num_row_B = size(B, 1);
 % rowZero = zeros(1, num_row_B);% tao vector 0
 % r = num_row_B;
