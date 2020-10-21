fa9195b2ccd16836f9c99c57170170b7b8ba0345
lamphucnghi313 <lamphucnghi@gmail.com>
2020-10-22 01:30:20 +0700

    no message

--- TH_Buoi3/Code/HamTinh_Tong_MaTran.m
+++
@@ -1,9 +0,0 @@
-function S = HamTinh_Tong_MaTran(A, n)
-% n=1 tinh tong theo cot, n=2 tinh tong theo dong cua ma tran.
-    if n == 1
-        S = sum(A);%hoac S = sum(A,1);
-    end
-    if n == 2
-        S = sum(A,2);
-    end
-end
