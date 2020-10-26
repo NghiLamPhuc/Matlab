44dea9e740942e11496bae014e7996463b296b7c
lamphucnghi313 <lamphucnghi@gmail.com>
2020-10-27 02:02:55 +0700

    no message

--- Bai_tap_thuc_hanh/BT_7/Code/BT_7.m
+++ Bai_tap_thuc_hanh/BT_7/Code/BT_7.m
@@ -8,6 +8,136 @@ close all
 format long
 
 % % ########################################## BT 7
+%% Bai tap 1
+% x = 0:0.5:4;
+% hold on;
+% plot(x, x);
+% plot(x, x.^3);
+% plot(x, exp(x));
+% plot(x, exp(x.^2));
+% xlabel('x'); ylabel('y');
+% legend('x', '{x^3}','{e^x}', '{e^{x^2}}');
+% title('Bai tap 1');
+% axis([-10 10 -2 10]);
+
+%% Bai tap 2
+% x = 0:0.5:4;
+% plot(x, exp(x.^2), '-.^b');
+% xlabel('x'); ylabel('y');
+% legend('{e^{x^2}}');
+% title('Bai tap 2');
+% axis([-1 3 0 10]);
+
+%% Bai tap 3
+% x = -5:1:5;
+% f_x = x./(1 + x.^4);
+% % set(0,'defaultTextInterpreter','latex');
+% plot(x, f_x, '-.or', 'lineWidth', 2, 'color', [0.6350 0.0780 0.1840],...
+%     'MarkerSize', 6,...
+%     'MarkerEdgeColor','k',...
+%     'MarkerFaceColor', 'b');
+% % %http://math.loyola.edu/~loberbro/matlab/html/colorsInMatlab.html
+% axis([-7 7 -7 7]);
+% xlabel('x'); ylabel('y');
+% title('bai tap 3');
+% legend('f(x) = x/(1 + {x^4})');
+% % legend('f(x) = \frac{x}{1+{x^4}}');
+
+%% Bai tap 4
+% hold on;
+% ezplot('sin(x)');
+% ezplot('cos(x)');
+% ezplot('tan(x)');
+% xlabel('x'); ylabel('y');
+% title('bai tap 4');
+% legend('sin(x)', 'cos(x)', 'tan(x)');
+
+%% Bai tap 5
+% x = -5:0.1:5;
+% 
+% y = x.^3 - 3.*x;
+% yd1 = 3.*x.^2 - 3; % yd1 = 0 -> x = +-1;yd1(-2) = 9; yd1()
+% yd2 = 6.*x;
+% 
+% yArr = [y; yd1; yd2];
+% 
+% hold on;
+% plot(x, yArr);
+% legend('y = {x^3} - 3x', 'y1d = 3{x^2} - 3', 'y2d = 6x');
+% xlabel('x'); ylabel('y');
+% title('Bai tap 5');
+%% Bai tap 6a
+% x = -10:10;
+% f = abs(x.^2 + 3.*x);
+% g = x.^3 - x - 2;
+% 
+% suptitle('Bai tap 6a');
+% subplot(1,2,1);
+% plot(x,f);
+% 
+% legend('|{x^2} + 3x|');
+% xlabel('x'); ylabel('y');
+% 
+% subplot(1,2,2);
+% plot(x,g);
+% 
+% legend('{x^3} - x - 2');
+% xlabel('x'); ylabel('y');
+
+%% Bai tap 6b
+% x = -10:10;
+% f = x.^2;
+% g = sqrt(x);
+% % set(0,'defaultTextInterpreter','latex');
+% 
+% suptitle('Bai tap 6b');
+% subplot(2,1,1);
+% plot(x,f);
+% 
+% legend('{x^2}');
+% xlabel('x'); ylabel('y');
+% 
+% subplot(2,1,2);
+% plot(x,g);
+% 
+% legend('{\surdx}');
+% xlabel('x'); ylabel('y');
+
+%% Bai tap 6c
+% x = -10:10;
+% % f = [exp(x); log(x); sin(x); cos(x)]
+% % set(0,'defaultTextInterpreter','latex');
+% 
+% suptitle('Bai tap 6c');
+% 
+% subplot(2,2,1);
+% plot(x,exp(x));
+% legend('{e^x}');
+% xlabel('x'); ylabel('y');
+% 
+% subplot(2,2,2);
+% plot(x,log(x));
+% legend('ln(x)');
+% xlabel('x'); ylabel('y');
+% 
+% subplot(2,2,3);
+% plot(x,sin(x));
+% legend('sin(x)');
+% xlabel('x'); ylabel('y');
+% 
+% subplot(2,2,4);
+% plot(x,cos(x));
+% legend('cos(x)');
+% xlabel('x'); ylabel('y');
+
+
+
+
+
+
+
+
+
 %% Bai tap 9
 % A = [-3 -3];
 % a = 5; b = 4;
--- Bai_tap_thuc_hanh/BT_7/README.md
+++ Bai_tap_thuc_hanh/BT_7/README.md
@@ -4,6 +4,17 @@ Ca 3-4 A504(đổi phòng A710) 16/10/2020
 
 Link download: https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/NghiLamPhuc/Matlab/tree/master/Bai_tap_thuc_hanh/BT_7/Code
 
+![App](./HinhAnh/Debai_HinhAnh/BT_7_1_2_3_4.JPG)
+
+![App](./HinhAnh/BaiLam_HinhAnh/GBT_7_1.JPG)
+![App](./HinhAnh/BaiLam_HinhAnh/GBT_7_1_ketqua.JPG)
+
+![App](./HinhAnh/BaiLam_HinhAnh/GBT_7_2.JPG)
+![App](./HinhAnh/BaiLam_HinhAnh/GBT_7_2_ketqua.JPG)
+
+![App](./HinhAnh/BaiLam_HinhAnh/GBT_7_4.JPG)
+![App](./HinhAnh/BaiLam_HinhAnh/GBT_7_4_ketqua.JPG)
+
 ![App](./HinhAnh/Debai_HinhAnh/BT_7_9.JPG)
 ![App](./HinhAnh/BaiLam_HinhAnh/GBT_7_9.JPG)
 ![App](./HinhAnh/BaiLam_HinhAnh/GBT_7_9_run.JPG)
