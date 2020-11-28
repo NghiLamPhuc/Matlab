f24f82415fc1565ed0178be985a1c1ef566ed878
lamphucnghi313 <lamphucnghi@gmail.com>
2020-11-28 19:32:05 +0700

    no message

--- Bai_tap_thuc_hanh/Final/matcos.m
+++ Bai_tap_thuc_hanh/Final/matcos.m
@@ -3,7 +3,7 @@ function S = matcos(A)
 S = A^0;
 k = 1;
 X = -0.5*A^2;
-while abs(max(X(:))) > 0.00001
+while abs(max(X(:))) > 0.000000000000001
     S = S + (-1) ^ k / factorial(2*k) * A^(2*k);
     k = k + 1;
     X = (-1) ^ k / factorial(2*k) * A^(2*k);
--- Bai_tap_thuc_hanh/Final/matsin.m
+++ Bai_tap_thuc_hanh/Final/matsin.m
@@ -3,7 +3,7 @@ function S = matsin(A)
 S = A; % k = 0
 k = 1;
 X = (-1/6) * A^3;% X = Sn+1(A) - Sn(A)
-while abs(max(X(:))) >= 0.01
+while abs(max(X(:))) >= 0.000000000000001
     S = S + X;
     k = k + 1;
     X = (-1)^k/factorial(2*k + 1)*A^(2*k + 1);
